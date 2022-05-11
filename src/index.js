f29b185a9bdcb56d4eb068cde60d2de3f4a4e9da
Ilya Bredihin <ilya.bredikhin@mail.ru>
2022-05-11 18:40:18 +0300

    text

--- src/index.js
+++ src/index.js
@@ -2,6 +2,4 @@ import * as images from "./js/images.js";
 import * as change__content from "./js/script__change__content.js";
 import * as questions__actor from "./js/script__questions.js";
 import * as score from "./js/script__score.js";
-import * as fullimgs from "./images/full";
-import * as imgs from "./images/img";
 import './css/style.css';
