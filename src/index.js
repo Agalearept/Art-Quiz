f4683e77554e02d91dc5094081e4aaae6bfdc5f5
Ilya Bredihin <ilya.bredikhin@mail.ru>
2022-05-11 18:21:36 +0300

    bundle

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-import {images} from "./js/images.js";
+import * as images from "./js/images.js";
 import * as change__content from "./js/script__change__content.js";
 import * as questions__actor from "./js/script__questions.js";
 import * as score from "./js/script__score.js";
+import * as fullimgs from "./images/full";
+import * as imgs from "./images/img";
+import './css/style.css';
